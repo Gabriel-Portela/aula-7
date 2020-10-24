38ca981df34dd3f16c1fa9eb8b87a329bedcdff8
Gabriel-Portela <70451796+Gabriel-Portela@users.noreply.github.com>
2020-10-24 12:55:07 -0300

    Add files via upload

---
+++ index.html/script.js
@@ -0,0 +1,16 @@
+let ano_atual=2020
+
+const nome = prompt("Olá qual o seu nome??")
+
+const nascimento = prompt("E o seu ano de nascimento??")
+
+let idade = ano_atual-nascimento
+
+
+alert(`olá seu nome é ${nome} e você tem ${idade} anos.`)
+
+
+
+
+
+
